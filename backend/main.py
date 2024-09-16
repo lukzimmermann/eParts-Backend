2c88bc7b0778013be75f49c5cfa8d13ceab52ac6
lukzimmermann <l.zimmermann@suter-industries.ch>
2024-09-16 09:33:44 +0200

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,6 @@ app = FastAPI(title="eParts",
 Organize your electronic components and projects with ease. 🚀
 """,
     version="0.0.1",
-    contact={
-        "name": "Lukas Zimmermann",
-        "email": "lukas.zimmermann@schoggipopcorn.ch",
-    },
     license_info={
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
